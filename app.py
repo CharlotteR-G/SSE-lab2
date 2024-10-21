09cdbb00e6893849986e864301085ec113704ea1
Charlotte Robinson-Green <cpr124@ic.ac.uk>
2024-10-21 13:43:45 +0100

    Add build.yml

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
 
 @app.route("/submit", methods=["POST"])
 def submit():
@@ -13,4 +16,12 @@ def submit():
     input_age = request.form.get("age")
     input_degree = request.form.get("degree-level")
     input_module = request.form.get("module")
-    return render_template("submission.html", first_name = input_first_name, last_name = input_last_name, age = input_age, student_number = input_student_number, degree_level = input_degree, module=input_module)
+    return render_template(
+        "submission.html",
+        first_name=input_first_name,
+        last_name=input_last_name,
+        age=input_age,
+        student_number=input_student_number,
+        degree_level=input_degree,
+        module=input_module,
+    )
