907fa94c4af564f6c5242b754105f71d080a4f13
Charlotte Robinson-Green <cpr124@ic.ac.uk>
2024-10-28 14:44:31 +0000

    revert name changes, reformatting

--- app_test.py
+++ app_test.py
@@ -3,9 +3,7 @@ from app import process_query
 
 def test_knows_about_dinosaurs():
     expected = "Dinosaurs ruled the Earth 200 million years ago"
-    assert (
-        process_query("dinosaurs") == expected
-    )
+    assert process_query("dinosaurs") == expected
 
 
 def test_does_not_know_about_asteroids():
